bb02946a91a7793c9853f17a13551beb943152ae
anchengjian <anchengjian@gmail.com>
2017-06-12 00:40:42 +0800

    feat: Custom outputFileName

--- build/build-win-setup.js
+++ build/build-win-setup.js
@@ -13,11 +13,11 @@ var config = require(path.resolve(rootPath, 'config'))
 var setupOptions = config.build.nw.setup
 var platforms = ['win32', 'win64']
 
-module.exports = function() {
+module.exports = function () {
   const res = []
   const files = fs.readdirSync(setupOptions.files)
 
-  files.forEach(function(fileName) {
+  files.forEach(function (fileName) {
     if (!~platforms.indexOf(fileName)) return
 
     const curPath = path.resolve(setupOptions.files, fileName)
@@ -36,11 +36,12 @@ module.exports = function() {
 function makeExeSetup(opt) {
   const { issPath, files, outputPath, outputFileName, resourcesPath, appPublisher, appURL, appId, platform } = opt
   const { name, appName, version } = tmpJson
-  const tmpIssPath = path.resolve(path.parse(issPath).dir, '_tmp_' + platform +'.iss')
+  const tmpIssPath = path.resolve(path.parse(issPath).dir, '_tmp_' + platform + '.iss')
+  const getOutputNameHandle = typeof outputFileName === 'function' ? outputFileName : getOutputName
 
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     // rewrite name, version to iss
-    fs.readFile(issPath, null, function(err, text) {
+    fs.readFile(issPath, null, function (err, text) {
       if (err) return reject(err)
 
       let str = iconv.decode(text, 'gbk')
@@ -48,7 +49,7 @@ function makeExeSetup(opt) {
         .replace(/_appName_/g, appName)
         .replace(/_version_/g, version)
         .replace(/_outputPath_/g, outputPath)
-        .replace(/_outputFileName_/g, getOutputName(outputFileName, { name, version, platform }))
+        .replace(/_outputFileName_/g, getOutputNameHandle({ name, version, platform }))
         .replace(/_filesPath_/g, files)
         .replace(/_resourcesPath_/g, resourcesPath)
         .replace(/_appPublisher_/g, appPublisher)
@@ -56,11 +57,11 @@ function makeExeSetup(opt) {
         .replace(/_appId_/g, appId)
 
 
-      fs.writeFile(tmpIssPath, iconv.encode(str, 'gbk'), null, function(err) {
+      fs.writeFile(tmpIssPath, iconv.encode(str, 'gbk'), null, function (err) {
         if (err) return reject(err)
 
         // inno setup start
-        innosetupCompiler(tmpIssPath, { gui: false, verbose: true }, function(err) {
+        innosetupCompiler(tmpIssPath, { gui: false, verbose: true }, function (err) {
           fs.unlinkSync(tmpIssPath)
           if (err) return reject(err)
           resolve(opt)
@@ -70,9 +71,6 @@ function makeExeSetup(opt) {
   })
 }
 
-function getOutputName(str, data) {
-  if (!str) return data.name
-  return str.replace(/\$\{(.*?)\}/g, function(a, b) {
-    return data[b] || b
-  })
+function getOutputName(data) {
+  return data.name
 }
